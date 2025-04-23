e5b68ca2a746f78955fab732215b4ee16f10d847
tinabenita <tina1717rego@gmail.com>
2025-04-23 14:51:56 +0530

    feat: created quiz form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuizFormComponent } from "./quiz-form/quiz-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuizFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/quiz-form/quiz-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-form.component.html',
+  styleUrl: './quiz-form.component.scss'
+})
+export class QuizFormComponent {
+
+}
