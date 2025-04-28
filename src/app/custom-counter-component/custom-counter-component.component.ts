95a4b2c265e4f0e461a351b4dda53c814818b3b1
tinabenita <tina1717rego@gmail.com>
2025-04-28 18:14:07 +0530

    task: Add appropriate comments to understand the ControlValueAccessor interface

--- src/app/custom-counter-component/custom-counter-component.component.ts
+++ src/app/custom-counter-component/custom-counter-component.component.ts
@@ -8,55 +8,89 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [MatIconModule],
   templateUrl: './custom-counter-component.component.html',
   styleUrl: './custom-counter-component.component.scss',
-  providers:[
+  providers: [
     {
-      provide: NG_VALUE_ACCESSOR,
+      provide: NG_VALUE_ACCESSOR, // Registers this component as a custom form control
       useExisting: CustomCounterComponentComponent,
-      multi: true
-    }
-  ]
+      multi: true, // Allows multiple providers for NG_VALUE_ACCESSOR
+    },
+  ],
 })
 export class CustomCounterComponentComponent implements ControlValueAccessor {
-  counter = 0;
-  touched = false;
-  disabled = false;
+  counter = 0; // Internal value of the custom control
+  touched = false; // Tracks whether the control has been touched
+  disabled = false; // Tracks whether the control is disabled
 
-  @Input() increment: number = 1;
+  @Input() increment: number = 1; // Input property to control the increment value
 
+  // Callback function to notify Angular when the value changes
   onChange = (counter: number) => {};
+
+  // Callback function to notify Angular when the control is touched
   onTouched = () => {};
 
+  /**
+   * Called when the user clicks the "Add" button.
+   * Increments the counter and notifies Angular of the new value.
+   */
   onAdd() {
-    this.markAsTouched();
-    this.counter += this.increment;
-    this.onChange(this.counter);
+    this.markAsTouched(); // Marks the control as touched
+    this.counter += this.increment; // Increment the counter
+    this.onChange(this.counter); // Notify Angular of the new value
   }
 
+  /**
+   * Called when the user clicks the "Remove" button.
+   * Decrements the counter and notifies Angular of the new value.
+   */
   onRemove() {
-    this.markAsTouched();
-    this.counter -= this.increment;
-    this.onChange(this.counter);
+    this.markAsTouched(); // Marks the control as touched
+    this.counter -= this.increment; // Decrement the counter
+    this.onChange(this.counter); // Notify Angular of the new value
   }
 
+  /**
+   * Called by Angular to set the value of the control.
+   * Updates the internal counter value.
+   * @param counter The new value to set
+   */
   writeValue(counter: number) {
     this.counter = counter;
   }
 
-  registerOnChange(onChange: any) {
+  /**
+   * Called by Angular to register a callback function that will be called
+   * when the control's value changes.
+   * @param onChange The callback function to register
+   */
+  registerOnChange(onChange: (counter: number) => void) {
     this.onChange = onChange;
   }
 
-  registerOnTouched(onTouched: any) {
+  /**
+   * Called by Angular to register a callback function that will be called
+   * when the control is touched.
+   * @param onTouched The callback function to register
+   */
+  registerOnTouched(onTouched: () => void) {
     this.onTouched = onTouched;
   }
 
+  /**
+   * Marks the control as touched and calls the registered `onTouched` callback.
+   */
   markAsTouched() {
     if (!this.touched) {
-      this.onTouched();
-      this.touched = true;
+      this.onTouched(); // Notify Angular that the control has been touched
+      this.touched = true; // Update the touched state
     }
   }
 
+  /**
+   * Called by Angular to enable or disable the control.
+   * Updates the `disabled` state of the control.
+   * @param disabled Whether the control should be disabled
+   */
   setDisabledState(disabled: boolean) {
     this.disabled = disabled;
   }
